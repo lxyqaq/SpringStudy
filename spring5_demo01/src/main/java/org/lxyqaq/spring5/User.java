0420aa9b5456d85f628ff84ebd5835439d044ef3
Xiangyu Liu <lxyqaq0128@gmail.com>
2021-01-01 17:59:18 +0000

    spring5_demo01

---
+++ spring5_demo01/src/main/java/org/lxyqaq/spring5/User.java
@@ -0,0 +1,16 @@
+package org.lxyqaq.spring5;
+
+/**
+ * @ClassName User
+ * @Description TODO
+ * @Author lxyqaq @Email A00279565@student.ait.ie
+ * @Date 2021/1/1 17:47
+ * @Version 1.0
+ */
+public class User {
+
+    public void add() {
+        System.out.println("add......");
+    }
+
+}
---
+++ spring5_demo01/src/test/java/org/lxyqaq/spring5/TestSpring5.java
@@ -0,0 +1,28 @@
+package org.lxyqaq.spring5;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @ClassName TestSpring5
+ * @Description TODO
+ * @Author lxyqaq @Email A00279565@student.ait.ie
+ * @Date 2021/1/1 17:53
+ * @Version 1.0
+ */
+public class TestSpring5 {
+
+    @Test
+    public void testAdd() {
+
+        //1.加载spring配置文件
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("bean1.xml");
+
+        //2.获取配置创建的对象
+        User user = applicationContext.getBean("user", User.class);
+
+        System.out.println(user);
+        user.add();
+    }
+}
