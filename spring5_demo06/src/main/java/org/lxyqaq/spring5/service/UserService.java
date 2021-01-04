4d7f509a3784c13257f4ba1e45061eab123106a6
Xiangyu Liu <lxyqaq0128@gmail.com>
2021-01-04 22:57:03 +0000

    spring5_demo05_数据库事务操作

--- spring5_demo06/src/main/java/org/lxyqaq/spring5/service/UserService.java
+++ spring5_demo06/src/main/java/org/lxyqaq/spring5/service/UserService.java
@@ -3,6 +3,7 @@ package org.lxyqaq.spring5.service;
 import org.lxyqaq.spring5.dao.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @ClassName UserService
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Service;
  * @Version 1.0
  */
 @Service
+@Transactional
 public class UserService {
 
     @Autowired
@@ -20,9 +22,16 @@ public class UserService {
     //转账的方法
     public void accountMoney() {
 
+        //第一步
         userDao.reduceMoney();
 
+        //模拟异常
+        int a = 10 / 0;
+
+        //第二步
         userDao.addMoney();
+
+        //出现异常，事务回滚
 
     }
 
