a533aa7d64a10e09180d06ac9f3cdef0fa0b725f
Xiangyu Liu <lxyqaq0128@gmail.com>
2021-01-05 15:09:17 +0000

    spring5_demo05_完全注解开发

--- spring5_demo06/src/main/java/org/lxyqaq/spring5/service/UserService.java
+++ spring5_demo06/src/main/java/org/lxyqaq/spring5/service/UserService.java
@@ -3,6 +3,8 @@ package org.lxyqaq.spring5.service;
 import org.lxyqaq.spring5.dao.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
@@ -13,7 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
  * @Version 1.0
  */
 @Service
-@Transactional
+@Transactional(readOnly = false, timeout = -1, propagation = Propagation.REQUIRED, isolation = Isolation.REPEATABLE_READ)
 public class UserService {
 
     @Autowired
--- spring5_demo06/src/test/java/TestSQL.java
+++ spring5_demo06/src/test/java/TestSQL.java
@@ -1,6 +1,8 @@
 import org.junit.Test;
+import org.lxyqaq.spring5.config.TxConfig;
 import org.lxyqaq.spring5.service.UserService;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -16,6 +18,17 @@ public class TestSQL {
     public void test1() {
 
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("bean1.xml");
+
+        UserService userService = applicationContext.getBean("userService", UserService.class);
+
+        userService.accountMoney();
+
+    }
+
+    @Test
+    public void test2() {
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(TxConfig.class);
 
         UserService userService = applicationContext.getBean("userService", UserService.class);
 
